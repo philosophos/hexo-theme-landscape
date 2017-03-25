91c4df8b00c91aa6e5b374955bb16e43d99a330c
philosophos <philosoph@yeah.net>
2017-03-25 13:59:02 +0800

    add Go To Top

--- source/js/script.js
+++ source/js/script.js
@@ -134,4 +134,43 @@
 
     $container.removeClass('mobile-nav-on');
   });
+
+  // totop
+    var totop = $("#totop"),
+        canvas = $("#totop-canvas"),
+        percent = $("#totop-percent"),
+        width = canvas.width(),
+        height = canvas.height(),
+        center = width / 2,
+        radius = parseInt((width - 3) / 2),
+        ctx = canvas[0].getContext("2d");
+    function draw_circle(color, percent) {
+        ctx.beginPath();
+        ctx.arc(center, center, radius, - Math.PI / 2, Math.PI * 1.5 * percent, false);
+        ctx.strokeStyle = color;
+        ctx.lineCap = 'round'; // butt, round or square
+        ctx.lineWidth = 3;
+        ctx.stroke();
+    }
+
+    totop.click(function() {
+        $("body, html").animate({
+            scrollTop: 0
+        }, 800);
+    });
+
+    $(window).scroll(function() {
+        var doc_height = $(document).height() - $(window).height(),
+            scroll_top = $(window).scrollTop(),
+            per = parseInt(scroll_top / doc_height * 100);
+        if (scroll_top >= 200) {
+            totop.addClass("display");
+            ctx.clearRect(0, 0, width, height);
+            draw_circle('#efefef', 1);
+            draw_circle('#009999', per/100);
+        } else
+            totop.removeClass("display");
+        percent.attr("data-percent", per);
+    });
+
 })(jQuery);
